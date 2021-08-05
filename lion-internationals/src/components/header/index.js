a13c44f7ecfe0574d383e4c8b22ceab0de0db1cc
Aditya Gupta <guptaaditya24@gmail.com>
2021-08-05 17:31:17 +0530

    Navbar cleanup.

--- lion-internationals/src/components/header/index.js
+++ lion-internationals/src/components/header/index.js
@@ -3,31 +3,25 @@ import { Navbar } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
 
-const headerMenu = [
-  {key: 1, href: `/users`, label: 'Users', exact: true},
-  {key: 2, href: `/groups`, label: 'Groups'},
-  {key: 3, href: `/user/add`, label: 'Add user', exact: true},
+const MenuItems = [
+  {key: 1, href: `/users`, label: 'Users', exact: true, isActive: false},
+  {key: 2, href: `/groups`, label: 'Groups', isActive: false},
+  {key: 3, href: `/user/add`, label: 'Add user', exact: true, isActive: false},
 ]
 
 const Header = (props) => {
-  let activeMenu = headerMenu.find(i => {
-    return (props.location.pathname.indexOf(i.href) === 0)
-  })
-  if(activeMenu) {
-    activeMenu = activeMenu.key
-  }
-  else activeMenu = 0
-  
   return (
     <Navbar>
       <Nav>
-      {headerMenu.map((i, index) => (
-        <NavLink to={i.href}>{i.label}</NavLink>
-      ))}
+        {MenuItems.map((i, index) => (
+          <NavLink to={i.href}>
+            {i.label}
+          </NavLink>
+        ))}
       </Nav>
     </Navbar>
   )
-};
+}
 
 const Nav = styled.div`
   display: flex;
@@ -41,6 +35,4 @@ const Nav = styled.div`
     font-size: 1.1em;
   }
 `
-
-
 export default Header
